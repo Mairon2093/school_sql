0ce938d9d4130725cac8e14c0127aacb46808b21
Mairon <144348349+Mairon2093@users.noreply.github.com>
2025-04-05 17:52:09 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import psycopg2
+import fastapi
+from fastapi import Request, Response, FastAPI
+
+database = "school_db"
+user = "school"
+password = "School1234*"
+host = "79.174.88.238"
+port = "15221"
+
+conn = psycopg2.connect(
+    database = database,
+    user = user,
+    password = password,
+    host = host,
+    port = port
+)
+
+cur = conn.cursor()
+migration = ""
+with open("migrations") as f:
+    migration = f.read()
+    cur.execute(migration)
+
+###
+
+app = FastAPI()
+
+@app.get("/ping")
+async def ping():
+    return Response(200)
+
+@app.post("/orders")
+async def register(r : Request):
+    print(await r.json())
